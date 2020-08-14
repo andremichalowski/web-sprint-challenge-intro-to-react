ac6adb6ca24b3beef172187c65ee57c93fbe4065
amichalo421 <andre.michalowski@gmail.com>
2020-08-14 11:57:24 -0400

    Styled

---
+++ starwars/src/components/Styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Styled = styled.div`
+    padding: 25px;
+    margin: 15px;
+    width: 450px;
+    color: white;
+    background-color: beige;
+    border: 2px dashed brown;
+    border-radius: 5px;
+    
+    h1 {
+        color: gray;
+    }
+
+    h2 {
+        color: lightgray;
+    }
+`;
+
+export default Styled;
