66437651b45d359c59305b342c0ca7f10bcac5cd
amichalo421 <andre.michalowski@gmail.com>
2020-08-14 12:22:18 -0400

    Accessing object w/JSON.stringify

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,14 +3,16 @@ import React from 'react';
 import Styled from './Styled';
 
 const Character = (props) => {
+  console.log(props.props[0]);
   return (
     <div>
       <Styled>
-        <h1> {props[1].name}</h1>
-        <h2> Birth Year: {props[1].birth_year}</h2>
+        
+        {/* <h1> {props.props[0].name}</h1> */}
+        {/* <h2> Birth Year: {props[1].birth_year}</h2>
         <h2> Home: {props[1].homeworld}</h2>
         <h2> Height: {props[1].height}</h2>
-        <h2> Mass: {props[1].mass}</h2>
+        <h2> Mass: {props[1].mass}</h2> */}
       </Styled>
     </div>
   );
--- starwars/src/components/StarWarsContainer.js
+++ starwars/src/components/StarWarsContainer.js
@@ -5,23 +5,31 @@ import Character from './Character';
 
 const StarWarsContainer = () => {
   const [starWarsData, setStarWarsData] = useState([]);
+  const stringVersion = JSON.stringify(starWarsData[0]);
 
   useEffect(() => {
     axios
       .get('https://swapi.py4e.com/api/people')
       .then(res => {
         console.log(res.data.results);
-        setStarWarsData(res.results);
+        setStarWarsData(res.data.results);
+          //Component wont render testing
+          console.log(res.data.results[0].name);
+          console.log(stringVersion);
+
+
+
       })
       .catch(err => {
         console.log('404 This is not the droid you are looking for', err);
       })
-  }, [starWarsData])
+  }, [])
 
   return (
     <div className="CardContainer">
       {/* <h1 className="Header">Characters</h1> */}
-      <Character props={starWarsData} />
+      {/* <Character props={starWarsData} /> */}
+      {/* <h1>{stringVersion}</h1> */}
 
     </div>
   );
