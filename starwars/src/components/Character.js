065bab95907bc8ecb826d9b9f3df76de71816605
amichalo421 <andre.michalowski@gmail.com>
2020-08-14 12:35:14 -0400

    Tast 2: MVP Completed

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,16 +3,15 @@ import React from 'react';
 import Styled from './Styled';
 
 const Character = (props) => {
-  console.log(props.props[0]);
+  
   return (
     <div>
       <Styled>
-        
-        {/* <h1> {props.props[0].name}</h1> */}
-        {/* <h2> Birth Year: {props[1].birth_year}</h2>
-        <h2> Home: {props[1].homeworld}</h2>
-        <h2> Height: {props[1].height}</h2>
-        <h2> Mass: {props[1].mass}</h2> */}
+        <h1> {props.character.name}</h1>
+        <h2> Birth Year: {props.character.birth_year}</h2>
+        <h2> Home: {props.character.homeworld}</h2>
+        <h2> Height: {props.character.height}</h2>
+        <h2> Mass: {props.character.mass}</h2>
       </Styled>
     </div>
   );
