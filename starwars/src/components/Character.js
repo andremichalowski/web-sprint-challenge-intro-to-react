f18519a022501c20e22221f70ff5a9fcd32172f2
amichalo421 <andre.michalowski@gmail.com>
2020-08-14 11:30:02 -0400

    Characters.js function added

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
-// Write your Character component here
+import React from 'react';
+
+function Character = (props) => {
+  return (
+    <div>
+    <h1>Test</h1>
+    {/* // <h1>Character Name: {props.name}</h1>
+    // <h1>Character {props.}</h1>
+    // <img src={props.img} /> */}
+    </div>
+  );
+}
+export default Character;
